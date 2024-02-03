1d10a5e0262933e94804b80a560298ea927f9589
Kamil Kucharczyk <kamil.kucharczyk007@gmail.com>
2024-02-03 19:18:23 +0100

    New branch, schema, service, files update

---
+++ controllers/contacts/createContact.js
@@ -0,0 +1,29 @@
+import { createContact, getContactByName } from "#service/index.js";
+
+async function createContacts(req, res, next) {
+  const { name, email, phone, favorite } = req.body;
+  try {
+    const existingContact = await getContactByName(name);
+
+    if (existingContact) {
+      return res.status(400).json({
+        status: "failed",
+        code: 400,
+        data: "Not found",
+        message: `Contact with name: '${name}' already exsist`,
+      });
+    } else {
+      const result = await createContact({ name, email, phone, favorite });
+      return res.status(201).json({
+        status: "success",
+        code: 201,
+        data: { contact: result },
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+}
+
+export { createContacts };
