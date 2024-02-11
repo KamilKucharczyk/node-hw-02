c51c478a42b2d52ecd7e42d2167f9c7be3f441d9
Kamil Kucharczyk <kamil.kucharczyk007@gmail.com>
2024-02-11 13:19:47 +0100

    Mail send, resend, verification, handler

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,18 +1,22 @@
 import express from "express";
 
 import auth from "#middlewares/authorization.js";
+import { verify } from "#controllers/users/verify.js";
 import { signUp } from "#controllers/users/auth.signupUser.js";
 import { logIn } from "#controllers/users/auth.loginUser.js";
 import { logOut } from "#controllers/users/auth.logoutUser.js";
 import { currentUser } from "#controllers/users/auth.currentUser.js";
 import { updateUserAvatar } from "#controllers/users/updateUserAvatar.js";
-import { upload } from "../../multerConfig/multerConfig.js";
+import { upload } from "#multerConfig/multerConfig.js";
+import { resendVerifyEmail } from "#controllers/users/resendVerify.js";
 
 const router = express.Router();
 
 router.post("/signup", signUp);
 router.post("/login", logIn);
+router.post("/verify", resendVerifyEmail);
 router.get("/logout", auth, logOut);
+router.get("/verify/:verificationToken", verify);
 router.get("/current", auth, currentUser);
 router.patch("/avatars", auth, upload.single("avatar"), updateUserAvatar);
 
